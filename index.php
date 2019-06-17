b82dc444eecbaba6b067821ff61b249d2fda53a3
camilaberti <camilabertialvesberti@gmail.com>
2019-06-17 17:20:38 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+//Criando uma variavel que contenha um array.
+$alunos = array("Brian", "Camila", "Ana", "Douglas");
+
+// Mostrando os valores do array conforme o valor do indice.
+echo $alunos[0]; //mostrará Brian
+echo "br />"; //apenas para quebrar a linha 
+echo $alunos[2]; //mostrara Ana
+
+//Adicionando um item ao array:
+$alunos[] = "Elisa";
+
+//Novo: função var_dump para descobrir o que há dentro de uma variável:
+echo "<hr />"; //apenas para criar uma linha separando os dados de cima.
+
+var_dump($alunos);
+
+echo "<hr />"; //apenas para criar uma linha separando os dados de cima.
+
+$logins = array( array("aluno", "123"), //0
+                 array("tiago", "etec"), //1
+                 array("user", "pass") //2
+                 );
+
+//var_dump($logins);
+
+echo $logins[2][0]; // mostra user
